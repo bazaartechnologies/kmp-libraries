4c16afc0f99e1e9c8a2df4aeb537e5ea7d4b8da8
Ammar Rizvi <ammar.rizvi@bazaartech.com>
2025-02-08 10:51:41 +0500

    added debounce and distinct in network connectivity observer

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,5 @@ plugins {
 
 subprojects {
     group = "com.tech.bazaar.kmp"
-    version = "2.2.2"
+    version = "2.2.3"
 }
--- network/src/commonMain/kotlin/com/tech/bazaar/network/api/InternetConnectivityNotifier.kt
+++ network/src/commonMain/kotlin/com/tech/bazaar/network/api/InternetConnectivityNotifier.kt
@@ -2,7 +2,10 @@ package com.tech.bazaar.network.api
 
 import dev.jordond.connectivity.Connectivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.mapLatest
 
@@ -16,15 +19,17 @@ interface InternetConnectivityNotifier {
 
 class DefaultInternetConnectivityNotifier private constructor(
     private val connectivity: Connectivity
-) :
-    InternetConnectivityNotifier {
-    @OptIn(ExperimentalCoroutinesApi::class)
-    override val statusUpdates = connectivity.statusUpdates.mapLatest {
-        when (it) {
-            is Connectivity.Status.Connected -> InternetConnectivityStatus.Connected(it.isMetered)
-            Connectivity.Status.Disconnected -> InternetConnectivityStatus.Disconnected
+) : InternetConnectivityNotifier {
+    @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+    override val statusUpdates = connectivity.statusUpdates
+        .distinctUntilChanged()
+        .debounce(1000L)
+        .mapLatest {
+            when (it) {
+                is Connectivity.Status.Connected -> InternetConnectivityStatus.Connected(it.isMetered)
+                Connectivity.Status.Disconnected -> InternetConnectivityStatus.Disconnected
+            }
         }
-    }
 
     override suspend fun isMonitoring(): Boolean? {
         return connectivity.isMonitoring.firstOrNull()
